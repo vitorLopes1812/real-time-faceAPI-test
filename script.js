c19e2bc2871057102f2a9a74cf3310a0c6225ef4
Vitor <vitor1184@hotmail.com>
2020-07-08 15:51:25 -0300

    terceiro_teste

---
+++ script.js
@@ -0,0 +1,39 @@
+const video = document.getElementById('video');
+
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+]).then(startVideo)
+
+function startVideo() {
+  navigator.getUserMedia(
+    { video: {} },
+    stream => video.srcObject = stream,
+    err => console.error(err)
+  )
+}
+
+video.addEventListener('play', () => {
+  const canvas = faceapi.createCanvasFromMedia(video)
+  document.body.append(canvas)
+  const displaySize = {
+    width: video.width,
+    height: video.height
+  }
+  faceapi.matchDimensions(canvas, displaySize)
+  console.log('video iniciado');
+  setInterval(async () => {
+    //configurar a detectação da face
+    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+    //teste para ver se está funcionando
+    console.log(detections)
+    //configurações da api
+    const resizeDetections = faceapi.resizeResults(detections, displaySize)
+    canvas.getContext('2d').clearRect(0,0,canvas.width, canvas.height)
+    faceapi.draw.drawDetections(canvas, resizeDetections)
+    faceapi.draw.drawFaceLandmarks(canvas, resizeDetections)
+    faceapi.draw.drawFaceExpressions(canvas, resizeDetections)
+  },100) //tempo em ms que é "autualizado"
+})
